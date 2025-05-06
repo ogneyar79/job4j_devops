83952dad56e8eb6a6377f8cbbadaa8dbbc75f509
ogneyar <maksimus.s@mail.ru>
2025-05-06 14:52:37 +0300

    [#505355] Использование кешей сборки в CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,30 @@
+import java.util.*
+
 rootProject.name = "DevOps"
-buildCache {
-    remote<HttpBuildCache> {
-        url = uri(System.getenv("GRADLE_REMOTE_CACHE_URL"))
-        isAllowInsecureProtocol = true
-        isAllowUntrustedServer = true
-        isPush = System.getenv("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
-        credentials {
-            username = System.getenv("GRADLE_REMOTE_CACHE_USERNAME")
-            password = System.getenv("GRADLE_REMOTE_CACHE_PASSWORD")
-        }
+
+// Загружаем .env (только локально)
+val envFile = file(".env")
+if (envFile.exists()) {
+    val props = Properties()
+    props.load(envFile.inputStream())
+    props.forEach { key, value ->
+        System.setProperty(key.toString(), value.toString())
     }
 }
 
+// Универсальная функция: сначала System properties, потом переменные среды
+fun envOrProp(key: String): String =
+    System.getProperty(key) ?: System.getenv(key) ?: error("Missing environment variable: $key")
+
+buildCache {
+    remote<HttpBuildCache> {
+        url = uri(envOrProp("GRADLE_REMOTE_CACHE_URL"))
+        isAllowInsecureProtocol = true
+        isAllowUntrustedServer = true
+        isPush = envOrProp("GRADLE_REMOTE_CACHE_PUSH").toBoolean()
+        credentials {
+            username = envOrProp("GRADLE_REMOTE_CACHE_USERNAME")
+            password = envOrProp("GRADLE_REMOTE_CACHE_PASSWORD")
+        }
+    }
+}
